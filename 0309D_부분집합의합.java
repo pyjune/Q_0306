f266ec1beb0f49817391beb4632c18ef259abaf7
pyjune <pyjune@naver.com>
2017-04-13 14:37:56 +0900

    Update 0309D_부분집합의합.java

--- 0309D_부분집합의합.java
+++ 0309D_부분집합의합.java
@@ -1,6 +1,3 @@
-// 모든 부분 집합을 만들어서 해결하는 경우..
-// 두 번째 테스트케이스까지만 적용 가능
-
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -40,12 +37,14 @@ public class Solution {
 	}
 	public static void subset_sum3(int n, int k, int sum_k, int s, int rs)
 	{
+		cnt2++;
 		int sum =0;
-		if(n==k)
+		if(s==sum_k)
 		{
-			cnt2++;
-			if(s==sum_k)
-				cnt++;
+			cnt++;
+		}
+		else if(n==k)
+		{
 			return;
 		}
 		else if(s>sum_k)
